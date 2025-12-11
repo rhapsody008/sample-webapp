d67d9cf88cd512cdda48e5706b1e1a7ece26cbb3
Zhou Yi <yi.zhou@nutanix.com>
2025-12-11 10:26:39 +0800

    update node to include access logs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(require('morgan')('dev'));
+
 // CHANGE THIS STRING to test your pipeline!
 const message = "Hello from Nutanix!"; 
 
